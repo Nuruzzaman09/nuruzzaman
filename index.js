883c755df17e6eff9021f21a0d121730457a6d5e
Nuruzzaman <111347411+Nuruzzaman09@users.noreply.github.com>
2022-11-30 21:25:59 +0600

    Add files via upload
    this is without project

---
+++ index.js
@@ -0,0 +1,10 @@
+function openMenu(){
+    const open = document.getElementById("open");
+    const content = document.getElementsByClassName("content");
+    open.style.height = "auto";
+    
+}
+function closemenu(){
+    const close= document.getElementById("open");
+    close.style.height = "0px";
+}
